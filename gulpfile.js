b2d5b21652a2bfa6ad7774f2daf7edc075e39733
bargester <mr.terenyak@gmail.com>
2018-05-14 15:39:33 +0300

    gulp-notify plugin gulp-sourcemaps plugin gulp-autoprefixer plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 'use strict';
 
-var gulp = require('gulp'),
-    pug  = require('gulp-pug'),
-    sass = require('gulp-sass'),
-    csso  = require('gulp-csso');
+var gulp        = require('gulp'),
+    pug         = require('gulp-pug'),
+    sass        = require('gulp-sass'),
+    csso        = require('gulp-csso'),
+    notify      = require ('gulp-notify'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    prefixer    = require('gulp-autoprefixer');
+
 
 gulp.task('pug', function () {
     return gulp.src('src/pug/pages/*.pug')
@@ -15,7 +19,15 @@ gulp.task('pug', function () {
 
 gulp.task('sass', function () {
     return gulp.src('src/static/styles/main.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(prefixer({
+            browsers: ['last 3 versions', '> 1%']
+        }))
+        .on("error", notify.onError({
+            title: "Error!"
+        }))
         .pipe(csso())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('build/static/css/'))
 });
